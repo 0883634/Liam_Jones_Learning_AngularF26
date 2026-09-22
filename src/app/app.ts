a56ce84fd67a62fd16f8b0c1f5cf076af9763acb
Jones <calibri2007@gmail.comgit config --global user.email calibri2007@gmail.com>
2026-09-22 15:54:59 -0400

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,51 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Game } from './shared/models/game';
+import { GameList } from './game-list/game-list';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameList],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected title = "game list";
+  protected title = 'game list';
 
   protected gameList: Game[] = [
-    {id: 1, name: "Minecraft", latestVer: 1.21, releaseDate: "nov 18, 2011", localPlayers: 4, onlinePlayers: 30},
-    {id: 2, name: "Knack", latestVer: 1.0, releaseDate: "nov 15, 2013", localPlayers: 2},
-    {id: 3, name: "Bloodborne", latestVer: 1.09, releaseDate: "mar 24, 2015", localPlayers: 1, onlinePlayers: 5},
-    {id: 4, name: "Hollow Knight", latestVer: 1.5, releaseDate: "feb 24, 2017", localPlayers: 1},
-    {id: 1, name: "Darksouls", latestVer: 1.03, releaseDate: "sept 22, 2011", localPlayers: 1, onlinePlayers: 6},
-    {id: 6, name: "No Man's Sky", latestVer: 4.7, releaseDate: "aug 9, 2016", localPlayers: 1, onlinePlayers: 32}
+    {
+      id: 1,
+      name: 'Minecraft',
+      latestVer: 1.21,
+      releaseDate: 'nov 18, 2011',
+      localPlayers: 4,
+      onlinePlayers: 30,
+    },
+    { id: 2, name: 'Knack', latestVer: 1.0, releaseDate: 'nov 15, 2013', localPlayers: 2 },
+    {
+      id: 3,
+      name: 'Bloodborne',
+      latestVer: 1.09,
+      releaseDate: 'mar 24, 2015',
+      localPlayers: 1,
+      onlinePlayers: 5,
+    },
+    { id: 4, name: 'Hollow Knight', latestVer: 1.5, releaseDate: 'feb 24, 2017', localPlayers: 1 },
+    {
+      id: 1,
+      name: 'Darksouls',
+      latestVer: 1.03,
+      releaseDate: 'sept 22, 2011',
+      localPlayers: 1,
+      onlinePlayers: 6,
+    },
+    {
+      id: 6,
+      name: "No Man's Sky",
+      latestVer: 4.7,
+      releaseDate: 'aug 9, 2016',
+      localPlayers: 1,
+      onlinePlayers: 32,
+    },
   ];
 }
